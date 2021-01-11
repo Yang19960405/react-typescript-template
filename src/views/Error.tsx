1b5c722ab098184c8fa9ee47856418ce1baed672
yangheng <495241237@qq.com>
2021-01-11 17:49:50 +0800

    登录页开发
    Signed-off-by: yangheng <495241237@qq.com>

---
+++ src/views/Error.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "antd";
+
+const ErrorPage404 = () =>{
+    return(
+        <>
+            <p>404</p>
+        </>
+    )
+}
+
+export default ErrorPage404
